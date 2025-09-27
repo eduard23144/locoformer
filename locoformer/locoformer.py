7adc4293497aced3e74aaff047819fe9eb00cbf8
lucidrains <lucidrains@gmail.com>
2025-09-27 08:12:30 -0700

    important ingredient

--- locoformer/locoformer.py
+++ locoformer/locoformer.py
@@ -1,6 +1,13 @@
+from functools import partial
+
 import torch
 import torch.nn.functional as F
-from torch.nn import Module, ModuleList
+from torch.nn import Module, ModuleList, Linear, RMSNorm, Identity
+
+from einops import einsum
+from einops.layers.torch import Rearrange
+
+LinearNoBias = partial(Linear, bias = False)
 
 # helper functions
 
@@ -9,6 +16,49 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+# transformer-xl with ppo
+
+class Attention(Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8,
+        pre_rmsnorm = True
+    ):
+        super().__init__()
+        self.norm = RMSNorm(dim) if pre_rmsnorm else Identity()
+        self.scale = dim_head ** -0.5
+        self.split_heads = Rearrange('b n (h d) -> b h n d', h = heads)
+        self.merge_heads = Rearrange('b h n d -> b n (h d)')
+
+        dim_inner = dim_head * heads
+        self.to_q = LinearNoBias(dim, dim_inner)
+        self.to_kv = LinearNoBias(dim, dim_inner * 2)
+        self.to_out = LinearNoBias(dim_inner, dim)
+
+    def forward(
+        self,
+        tokens,
+    ):
+        tokens = self.norm(tokens)
+
+        q, k, v = (self.to_q(tokens), *self.to_kv(tokens).chunk(2, dim = -1))
+
+        q, k, v = map(self.split_heads, (q, k, v))
+
+        q = q * self.scale
+
+        sim = einsum(q, k, 'b h i d, b h j d -> b h i j')
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum(attn, v, 'b h i j, b h j d -> b h i d')
+
+        out = self.merge_heads(out)
+
+        return self.to_out(out)
 
 # class
 
